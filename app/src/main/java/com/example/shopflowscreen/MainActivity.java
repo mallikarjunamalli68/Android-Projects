5873782f1b7b669cf7cd93bd012f16fd745fab7d
mallikarjunamalli68 <talarimallikarjuna408@gmail.com>
2025-05-25 22:44:07 +0530

    Update MainActivity.java
    Updated Shop Flow Screen

--- app/src/main/java/com/example/shopflowscreen/MainActivity.java
+++ app/src/main/java/com/example/shopflowscreen/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shopflowscreen;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.shopflowscreen.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.oldPrice.setPaintFlags(binding.oldPrice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        binding.oldPrice2.setPaintFlags(binding.oldPrice2.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
     }
 }
